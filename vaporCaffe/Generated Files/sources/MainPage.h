68cfa5efaba7263a71ee2e22081d57ddb7183587
Jack Sims <24850877+jackpots28@users.noreply.github.com>
2019-02-06 22:46:50 -0600

    Add project files.

---
+++ vaporCaffe/Generated Files/sources/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::vaporCaffe::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::vaporCaffe::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
---
+++ vaporCaffe/MainPage.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include <windows.Applicationmodel.Payments.h>
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::vaporCaffe::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+
+	int counter = 0;
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+		if (counter % 2 == 0) {
+			myButton().Content(box_value(L"Clicked\n"));
+		}
+		else {
+			myButton().Content(box_value(L"Click Me"));
+		}
+		counter += 1;
+    }
+}
